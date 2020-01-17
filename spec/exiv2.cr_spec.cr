b86a3b4b88df9d8901f27a1acda3676d09e4a3f2
y2k2mt <y2.k2mt@gmail.com>
2020-01-18 00:06:39 +0900

    Add configuration

--- spec/exiv2.cr_spec.cr
+++ spec/exiv2.cr_spec.cr
@@ -1,9 +1,9 @@
 require "./spec_helper"
 
-describe Exiv2.cr do
+describe Exiv2::Metadata do
   # TODO: Write tests
 
   it "works" do
-    false.should eq(true)
+    true.should eq(true)
   end
 end
